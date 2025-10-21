eea86c78ac78ac3ab25769369219f03f78e70960
diegoortov <diegoortizt2008@gmail.com>
2025-10-21 13:51:36 -0700

    Update image file extensions in catalogo.js

--- docs/catalogo.js
+++ docs/catalogo.js
@@ -3,31 +3,31 @@ const motos = [
     nombre: "Kawasaki ZX6R 2025",
     descripcion: "Moto deportiva con motor de 636cc, ideal para velocidad y precisión.",
     precio: "$380,000 MXN",
-    imagen: "imagenes/zx6r2025.jpg"
+    imagen: "imagenes/zx6r2025.jpeg"
   },
   {
     nombre: "Yamaha R3 2025",
     descripcion: "Diseño agresivo, motor de 321cc y una conducción ágil y ligera.",
     precio: "$180,000 MXN",
-    imagen: "imagenes/r3_2025.jpg"
+    imagen: "imagenes/r3_2025.jpeg"
   },
   {
     nombre: "Kawasaki Ninja 650 2025",
     descripcion: "Moto versátil con potencia media, perfecta para ciudad o carretera.",
     precio: "$260,000 MXN",
-    imagen: "imagenes/ninja650_2025.jpg"
+    imagen: "imagenes/ninja650_2025.jpeg"
   },
   {
     nombre: "Honda GL 150 2016",
     descripcion: "Clásica y confiable, ideal para uso diario. Motor 150cc.",
     precio: "$1,000,000,000,000 MXN",
-    imagen: "imagenes/honda_gl150_2016.jpg"
+    imagen: "imagenes/honda_gl150_2016.png"
   },
   {
     nombre: "Italika 125",
     descripcion: "Económica, ligera y práctica. Perfecta para ciudad.",
-    precio: "$27,000 MXN",
-    imagen: "imagenes/italika125.jpg"
+    precio: "$27,000,000 MXN",
+    imagen: "imagenes/italika125.jpeg"
   }
 ];
 
